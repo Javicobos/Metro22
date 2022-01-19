d0033eb9d09ff6e9632e95c41cded4b506916cbe
Javier Cobos del alamo <jcobos-d@c1r12s6.42madrid.com>
2022-01-19 20:32:20 +0100

    everything is now faster thanks to tuples

--- Metro0.py
+++ Metro0.py
@@ -54,26 +54,42 @@ FullMetro = [L1, L2, L3, L4, L5, L6, L7]
 #	Let's try:
 
 connections = []
+allStationsSimplelist = []
 
 def isStationThere(station):
 	for i,sublist in enumerate(connections):
-		if sublist[0] == station:
+		if sublist[0][0] == station:
 			return i
 	return -1
 
 
 def makeConnections():
 	for line in FullMetro:
+		for station in line:
+			if station not in allStationsSimplelist:
+				allStationsSimplelist.append(station)
+	for line in FullMetro:
 		lastStationIndex = len(line) - 1
 		for e,station in enumerate(line):
 			#if not any(connections[0] == station for l in connections) # I think this is kinda cool but probably hard to read
 			stationIndex = isStationThere(station)
 			if stationIndex == -1:
-				connections.append([station])
-			if e > 0  and line[e-1] not in connections[stationIndex]:
-				connections[stationIndex].append(line[e-1])
-			if e < lastStationIndex and line[e+1] not in connections[stationIndex]:
-				connections[stationIndex].append(line[e+1])
+				stationIndex = allStationsSimplelist.index(station)
+				connections.append([(station, stationIndex)])
+			else:
+				stationIndex = allStationsSimplelist.index(station)
+			if e > 0:
+				for i,subtuple in enumerate(connections[stationIndex]):
+					if subtuple[0] == line[e-1]:
+						break
+				else:
+					connections[stationIndex].append((line[e-1], allStationsSimplelist.index(line[e-1])))
+			if e < lastStationIndex:
+				for i,subtuple in enumerate(connections[stationIndex]):
+					if subtuple[0] == line[e+1]:
+						break
+				else:
+					connections[stationIndex].append((line[e+1], allStationsSimplelist.index(line[e+1])))
 				
 
 			
@@ -88,8 +104,8 @@ outputFile = open('Metro0.txt', 'w') #should we sort connections?
 #connections.sort()
 for i in connections: #export LC_CTYPE="es:ES.UTF-8" //that did something but not quite what I wanted
 	for e in i[:-1]:
-		outputFile.write(e + ' | ')
-	outputFile.write(i[-1] + '\n')
+		outputFile.write(e[0] + ' | ')
+	outputFile.write(i[-1][0] + '\n')
 
 def connectTwoStations(station1, station2): #for now this will be gigaslow but let's just make it work
 	bread = []		#for breadth lol
@@ -168,23 +184,26 @@ def longestFromStation(station):
 	#	return False
 	bread = []
 	trips = 0
-	bread.append([station])
-	foundStationsSimpleList = [station]
+	ogIndex = allStationsSimplelist.index(station)
+	bread.append([(station, ogIndex)])
+	foundStationsSimpleList = [ogIndex]
 	foundStations = 1
-	totalStations = len(connections)
+	totalStations = len(allStationsSimplelist)
 	while True:
 		nextTripLevel = []
 		for stationVisited in bread[trips]:
-			for neighbor in connections[isStationThere(stationVisited)][1:]:
-				if neighbor not in foundStationsSimpleList:
+			for neighbor in connections[stationVisited[1]][1:]:
+				if neighbor[1] not in foundStationsSimpleList:
 					nextTripLevel.append((neighbor))
-					foundStationsSimpleList.append(neighbor)
+					foundStationsSimpleList.append(neighbor[1])
 					foundStations += 1
 					if foundStations == totalStations:
-						return neighbor, trips +1
+						return neighbor[0], trips +1
 		bread.append(nextTripLevel)
 		trips += 1
 
+
+cProfile.run('longestFromStation("Casa de Campo")')
 
 def getLongestTripSLow(): #very slow
 	maxTrips = 0
@@ -210,22 +229,20 @@ def getLongestTripFast(): #fine but can be faster, slows down a lot with more st
 	
 def getLongestTripFastER():
 	maxTrips = 0
-	candidates = []
-	for station in connections:
-		candidates.append(station[0])
+	candidates = allStationsSimplelist[:]
 	for neighList in connections:
 		if len(neighList) == 2: #and neighList[0] in candidates:
-			oldStation = neighList[0]
-			notCandidate = neighList[1]
-			neighbors = connections[isStationThere(notCandidate)][1:]
+			oldStation = neighList[0][0]
+			notCandidate = neighList[1][0]
+			neighbors = connections[neighList[1][1]][1:]
 			while len(neighbors) <= 3:
 				candidates.pop(candidates.index(notCandidate))
 				#print notCandidate
 				for newNei in neighbors:
-					if newNei != oldStation:
+					if newNei[0] != oldStation:
 						oldStation = notCandidate
-						notCandidate = newNei
-						neighbors = connections[isStationThere(notCandidate)][1:]
+						notCandidate = newNei[0]
+						neighbors = connections[newNei[1]][1:]
 						break
 	#now with a reduced list of candidates we apply the bread
 	#print candidates
@@ -247,3 +264,7 @@ def getLongestTripFastER():
 #cProfile.run('getLongestTripFast()')
 
 cProfile.run('getLongestTripFastER()')
+
+#major update: "isStationThere" is taking a lot of computing space when it's basically a function to get indexes
+#we're just going to store stations as tuples so they have their index next to themselves, ready to be accessed
+# done! this has increased setup time a bit maybe (<0.001 anyway), but time to get the longest trip is almost halved
